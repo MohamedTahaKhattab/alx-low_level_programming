10ddbe20ab0fb25c452019f0d2692fc4a26f38cc
Mohamed Khattab <mtahakhattab98@gmail.com>
2023-03-16 19:36:11 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,10 @@
 #include<stdio.h>
+/*
+ * main - Entry point
+ *
+ * Description: This my first program in C
+ *
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
