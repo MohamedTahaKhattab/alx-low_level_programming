7df35df8ab51a5df47b0cedfc8392b6979e96360
Mohamed Khattab <mtahakhattab98@gmail.com>
2023-03-16 20:05:15 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,9 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
- * main - Entry point
- *
- * Description: A C program that prints with put function
- * 
- * Return: Alawys 0 (Success)
+ * main -Entry point
+ * Description: A C program that prints with printf function
+ * Return: Always 0 (Success)
 */
 
 int main(void)
